aaa28243cd603103328e9930669cbe095d3f5dec
Benny Hernandez <mail@bennyhernandez.com>
2022-03-08 22:37:29 -0500

    added cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors();
+  app.enableCors({
+    origin: process.env.FRONT_URL,
+    credentials: true,
+  });
   const port = process.env.PORT || 3001;
   await app.listen(port);
 }
