bc36a27f316fb44b63005b09dd5740421103ec09
Benny Hernandez <mail@bennyhernandez.com>
2022-03-08 20:16:09 -0500

    Refactor: database configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,7 +14,12 @@ import { NotesModule } from './notes/notes.module';
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => {
+        const isProduction = configService.get('STAGE') === 'prod';
         return {
+          ssl: isProduction,
+          extra: {
+            ssl: isProduction ? { rejectUnauthorized: false } : null,
+          },
           type: 'postgres',
           host: configService.get('DB_HOST'),
           port: configService.get('DB_PORT'),
